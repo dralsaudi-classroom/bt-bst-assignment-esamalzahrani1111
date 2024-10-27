7c82d79f47b5a083ec9d31d89d53b4e8d03d08f9
esamalzahrani1111 <444100946@student.ksu.edu.sa>
2024-10-27 16:33:00 +0300

    Update TreeUser.java

--- src/main/java/com/example/project/TreeUser.java
+++ src/main/java/com/example/project/TreeUser.java
@@ -12,7 +12,6 @@ public class TreeUser {
 	    int leftcount = 0;
 	    int rightcount=0;
 	    if(bt.isLeaf()) {
-		    bt.find(Relative.Parent);
 		    return 1;
 	    }
 	    if(bt.find(Relative.RightChild)){
