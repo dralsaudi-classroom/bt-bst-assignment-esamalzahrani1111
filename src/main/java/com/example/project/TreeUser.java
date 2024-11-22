77af721e8a7967583f35a8b633b250ba42dde5b8
esamalzahrani1111 <444100946@student.ksu.edu.sa>
2024-11-22 15:18:13 +0300

    Update TreeUser.java

--- src/main/java/com/example/project/TreeUser.java
+++ src/main/java/com/example/project/TreeUser.java
@@ -7,24 +7,24 @@ public class TreeUser {
         // assume the following method exists in the ADT: isLeaf (boolean flag): requires: Binary tree is not empty.
         // input: None. results: if the current node of the binary tree is a leaf then flag is set to true otherwise it is set to false. output: flag.
         // you can't call countLeaves from BT.jav
-	    if(bt.empty())
-		    return 0;
-	    int leftcount = 0;
-	    int rightcount=0;
-	    if(bt.isLeaf()) {
-		    return 1;
-	    }
-	    if(bt.find(Relative.RightChild)){
-	rightcount = countLeaves(bt);
-		    bt.find(Relative.Parent);
-	    }
-	    if(bt.find(Relative.LeftChild)){
-		    leftcount = countLeaves(bt);
-		     bt.find(Relative.LeftChild);
-    }
-    return leftcount+rightcount;
+	//     if(bt.empty())
+	// 	    return 0;
+	//     int leftcount = 0;
+	//     int rightcount=0;
+	//     if(bt.isLeaf()) {
+	// 	    return 1;
+	//     }
+	//     if(bt.find(Relative.RightChild)){
+	// rightcount = countLeaves(bt);
+	// 	    bt.find(Relative.Parent);
+	//     }
+	//     if(bt.find(Relative.LeftChild)){
+	// 	    leftcount = countLeaves(bt);
+	// 	     bt.find(Relative.LeftChild);
+ //    }
+ //    return leftcount+rightcount;
     
-    }
-	    
+ //    }
+	    BTNode<T> temp = bt.current;
 	
 }
